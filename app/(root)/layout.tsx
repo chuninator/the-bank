c8569c7436fda170fa5e4d21118a89aef98e2468
chuninator <mchun2121@gmail.com>
2024-05-06 12:57:31 -0700

    sidebar label not full working links, styled correctly

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Matt", lastName: "Chun" };
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   );
