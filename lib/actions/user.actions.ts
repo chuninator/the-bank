ea7ac4d93d7a9b8876ff0535d5d5c892ba449d05
chuninator <mchun2121@gmail.com>
2024-05-16 11:24:34 -0700

    sign up functionality added destructure userData elements to passthrough

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,9 @@
 "use server";
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+
 export const signIn = async () => {
   try {
   } catch (error) {
@@ -9,7 +13,34 @@ export const signIn = async () => {
 
 export const signUp = async (userData: SignUpParams) => {
   try {
+    //destructure
+    const { email, password, firstName, lastName } = userData;
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
   } catch (error) {
     console.log(error);
   }
 };
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
